b2df1313dd8310ee63977f6cc73bb3cc260a21ce
Will <adamwd1@student.op.ac.nz>
2026-03-24 14:21:10 +1300

    build: started a basic project. working on player movement

---
+++ heat-test/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+
+
+//Basic player movement script
+public class PlayerController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
