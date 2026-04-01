bb71dcebb2c48e10986b9a166f36a99d2a27391c
JayDib89 <everjr1@student.op.ac.nz>
2026-04-01 20:38:52 +1300

    Feat: (CS) Coal added to HUD, Coal deposit now adds coal. Furnace reduces players energy loss.

--- Heat/Assets/_Scripts/HudManager.cs
+++ Heat/Assets/_Scripts/HudManager.cs
@@ -6,8 +6,12 @@ public class HudManager : MonoBehaviour
     public PlayerController playerController;
     public TextMeshProUGUI HeatEnergy;
 
+     public TextMeshProUGUI HeatCoal;
+    
+
     void Update()
     {
         HeatEnergy.text = "Energy: " + playerController.currentHealth;
+        HeatCoal.text = "Energy: " + playerController.playerCoal;
     }
 }
