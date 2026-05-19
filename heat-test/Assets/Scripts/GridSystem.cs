876e8540fc1754fc54ad8cebdd65d4b84b8d1be9
Will <adamwd1@student.op.ac.nz>
2026-05-19 13:39:09 +1200

    feat: added grid system

--- heat-test/Assets/Scripts/GridSystem.cs
+++ heat-test/Assets/Scripts/GridSystem.cs
@@ -1,19 +1,101 @@
+﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 
 
 //Code for the grid used in base customisation
 public class GridSystem : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public GameObject objectToPlace;
+    public float gridSize = 1f;
+    private GameObject ghostObject;
+    private HashSet<Vector3> occupiedPositions = new HashSet<Vector3>();
+
+    private void Start()
     {
-        
+        CreateGhostObject();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        UpdateGhostPosition();
+
+        // ↓ Replace Input.GetMouseButtonDown(0)
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            PlaceObject();
+        }
+    }
+
+    void UpdateGhostPosition()
+    {
+        // ↓ Replace Input.mousePosition
+        Vector2 mousePos = Mouse.current.position.ReadValue();
+        Ray ray = Camera.main.ScreenPointToRay(mousePos);
+
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            Vector3 point = hit.point;
+            Vector3 snappedPosition = new Vector3(
+                Mathf.Round(point.x / gridSize) * gridSize,
+                Mathf.Round(point.y / gridSize) * gridSize,
+                Mathf.Round(point.z / gridSize) * gridSize
+            );
+            ghostObject.transform.position = snappedPosition;
+
+            if (occupiedPositions.Contains(snappedPosition))
+                SetGhostColor(Color.red);
+            else
+                SetGhostColor(Color.green);
+        }
+    }
+
+    void CreateGhostObject()
+    {
+        ghostObject = Instantiate(objectToPlace);
+        ghostObject.GetComponent<Collider>().enabled = false;
+
+        Renderer[] renderers = ghostObject.GetComponentsInChildren<Renderer>();
+
+        foreach (Renderer renderer in renderers)
+        {
+            Material mat = renderer.material;
+            Color color = mat.color;
+            color.a = 0.5f;
+            mat.color = color;
+
+            mat.SetFloat("_Mode", 2);
+            mat.SetInt("_ScrBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+            mat.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcColor);
+            mat.SetInt("_ZWrite", 0);
+            mat.DisableKeyword("_ALPHATEST_ON");
+            mat.EnableKeyword("_ALPHABLEND_ON");
+            mat.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+            mat.renderQueue = 3000;
+        }
+    }
+
+    void SetGhostColor(Color color)
+    {
+        Renderer[] renderers = ghostObject.GetComponentsInChildren<Renderer>();
+
+        foreach(Renderer renderer in renderers)
+        {
+            Material mat = renderer.material;
+            mat.color = color;
+        }
+    }
+
+    void PlaceObject()
+    {
+        Vector3 placementPosition = ghostObject.transform.position;
+
+        if (!occupiedPositions.Contains(placementPosition))
+        {
+            Instantiate(objectToPlace, placementPosition, Quaternion.identity);
+
+            occupiedPositions.Add(placementPosition);
+        }
     }
 }
